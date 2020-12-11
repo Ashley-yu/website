4dcb0f465fa8e1e62117db85449a6e7646510a15
Ashley <ashley@championtek.com.tw>
2020-12-11 17:30:44 +0800

    Add Owl Carousel

--- src/components/Carousel/OwlCarousel.vue
+++ src/components/Carousel/OwlCarousel.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
-  .container-fluid
+  .container-fluid.p-0
+    section
+      carousel(:autoplay="true")
+        img(src='https://placeimg.com/200/200/any?1')
+        img(src='https://placeimg.com/200/200/any?2')
+        img(src='https://placeimg.com/200/200/any?3')
+        img(src='https://placeimg.com/200/200/any?4')
+        img(src='https://placeimg.com/200/200/any?5')
+        img(src='https://placeimg.com/200/200/any?6')
+        img(src='https://placeimg.com/200/200/any?7')
+        img(src='https://placeimg.com/200/200/any?8')
+        img(src='https://placeimg.com/200/200/any?9')
+    footer.bg-primary.text-light.text-center.p-5
+      p Reference:
+      ul.list-unstyled
+        li
+          a.text-info(href='https://github.com/s950329/vue-owl-carousel') vue-owl-carousel
 </template>
 
 <script>
+import carousel from 'vue-owl-carousel'
+
 export default {
-name: "OwlCarousel"
+  name: "OwlCarousel",
+  components: {
+    carousel
+  },
 }
 </script>
 
